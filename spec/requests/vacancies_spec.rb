a68d26a9e380a5215b1261a8db9f2fec13962d66
Nelly Simkova <sk8high3r@ya.ru>
2023-09-20 22:17:31 +0300

    Remove helper spec and VacanciesController tests

--- spec/requests/vacancies_spec.rb
+++ spec/requests/vacancies_spec.rb
@@ -1,52 +1,5 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Vacancies", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/vacancies/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/vacancies/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/vacancies/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/vacancies/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/vacancies/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/vacancies/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /delete" do
-    it "returns http success" do
-      get "/vacancies/delete"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
