003787f715386819b2d80c49381dc5e31371169e
odanetpro <odanetpro@gmail.com>
2023-09-21 22:08:23 +0900

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_061902) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_130454) do
   create_table "contact_types", force: :cascade do |t|
     t.string "name", null: false
     t.string "icon", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_061902) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
