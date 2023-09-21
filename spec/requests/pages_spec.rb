a46da26498c9d62fe9173628ca69de51b78953e7
Dmitry Proskurekov <proskurekovdim@gmail.com>
2023-09-21 19:52:10 +0700

    remove frozen_string_literal from pages_spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
+require "rails_helper"
 
-require 'rails_helper'
-
-describe 'GET /welcome' do
-  it 'returns http status success' do
+describe "GET /welcome" do
+  it "returns http status success" do
     get root_url
     expect(response).to have_http_status(:success)
   end
