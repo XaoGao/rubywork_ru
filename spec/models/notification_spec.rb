aca7fef4ef2424ff0cd0df6ccaf56097785dd88d
Makhmadiyorov Fakhriddin <makhmadiyorovfakhriddin@gmail.com>
2023-09-27 18:25:26 +0500

    replaced single quotes with double ones

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -10,7 +10,7 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Notification, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
