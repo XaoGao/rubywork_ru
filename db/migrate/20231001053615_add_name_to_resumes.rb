84e67340d79ec7f2f1da18406238fb691293ca1b
Makhmadiyorov Fakhriddin <makhmadiyorovfakhriddin@gmail.com>
2023-10-01 10:56:56 +0500

    added default value for name

--- db/migrate/20231001053615_add_name_to_resumes.rb
+++ db/migrate/20231001053615_add_name_to_resumes.rb
@@ -1,5 +1,5 @@
 class AddNameToResumes < ActiveRecord::Migration[7.0]
   def change
-    add_column :resumes, :name, :string, null: false
+    add_column :resumes, :name, :string, null: false, default: ""
   end
 end
