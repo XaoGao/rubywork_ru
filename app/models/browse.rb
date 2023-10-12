9a4a0fa34383d3d96487da2dc624705827d5d1d9
Nelly Simkova <sk8high3r@ya.ru>
2023-10-12 11:06:01 +0300

    Generate Browse model

---
+++ app/models/browse.rb
@@ -0,0 +1,4 @@
+class Browse < ApplicationRecord
+  belongs_to :user
+  belongs_to :vacancy
+end
---
+++ spec/factories/browses.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :browse do
+    user { nil }
+    vacancy { nil }
+  end
+end
