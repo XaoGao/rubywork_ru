751abf42e4c250f379eabde1e2f52a2e30477f03
Nelly Simkova <sk8high3r@ya.ru>
2023-10-12 12:59:29 +0300

    Add annotate

--- spec/factories/browses.rb
+++ spec/factories/browses.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: browses
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  vacancy_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :browse do
     user { nil }
