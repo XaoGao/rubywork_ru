beda3877f29ee87e63622fbbf241c9dc4a7b7d90
Nelly Simkova <sk8high3r@ya.ru>
2023-10-13 13:50:17 +0300

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -125,7 +125,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_080842) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "browses_count"
+    t.integer "browses_count", default: 0
     t.index ["user_id"], name: "index_vacancies_on_user_id"
   end
 
