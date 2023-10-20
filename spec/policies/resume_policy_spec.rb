25fb3d072cbbd6a09d3e49aef3de53ede7156c72
Makhmadiyorov Fakhriddin <makhmadiyorovfakhriddin@gmail.com>
2023-10-20 20:41:29 +0500

    rewrote resume spec in RSpec DSL

--- spec/policies/resume_policy_spec.rb
+++ spec/policies/resume_policy_spec.rb
@@ -2,21 +2,16 @@ require "rails_helper"
 
 RSpec.describe ResumePolicy do
   let(:user)    { build_stubbed(:user, :company) }
-  let(:resume)  { build_stubbed(:resume, user:) }
+  let(:record)  { build_stubbed(:resume, user:) }
 
-  let(:policy) { described_class.new(resume, user:) }
+  let(:context) { { user: } }
 
-  describe "#new?" do
-    subject { policy.apply(:new?) }
+  describe_rule :new? do
+    succeed "when user is company"
 
-    context "when the user is a company, it returns true" do
-      it { is_expected.to be true }
-    end
-
-    context "when the user is admin, it returns false" do
+    failed "when user is admin" do
       let(:user) { build_stubbed(:user, :admin) }
-
-      it { is_expected.to be false }
+      let(:record) { build_stubbed(:resume) }
     end
   end
 end
