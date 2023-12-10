a38734f42e1ad3a2566990ff65ddc859336f6f36
Tavbulatov <alikhantam26@gmail.com>
2023-12-10 12:56:25 +0300

    added show of flash message in application.html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
       page_title
     end
   end
+
+  def flash_message_css_class(flash_type)
+    case flash_type
+    when "notice" then "alert alert-info"
+    when "success" then "alert alert-success"
+    when "error" then "alert alert-danger"
+    when "alert" then "alert alert-warning"
+    end
+  end
 end
