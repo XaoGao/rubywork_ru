447b8693d9465b209e5cb661a597f4c31094fb63
Tavbulatov <alikhantam26@gmail.com>
2023-12-16 14:31:49 +0300

    change factorio Reaction

--- spec/factories/reactions.rb
+++ spec/factories/reactions.rb
@@ -12,9 +12,9 @@
 #
 FactoryBot.define do
   factory :reaction do
-    user { nil }
-    vacancies { nil }
+    user
+    vacancies
     readed_at { "2023-12-13 22:30:16" }
-    cover_letter { "MyText" }
+    cover_letter { Faker::Lorem.sentence(word_count: 100) }
   end
 end
