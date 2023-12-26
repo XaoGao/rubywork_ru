fbfa30f6c2c31cbfffb745b6c8d3ba0ad5c24f11
secretpray <a.p.ruban@gmail.com>
2023-12-26 22:55:36 +0200

    add user display name

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,7 +1,7 @@
 module NavbarHelper
   def selected_language_icon
     Rails.cache.fetch("selected_language_name/#{current_locale}", expires_in: 1.day) do
-      image_tag "icons/#{current_locale}.svg", size: "24", title: t("navbar.language")
+      image_tag "icons/#{current_locale}.svg", size: "23", class: "rounded-2", title: t("navbar.language")
     end
   end
 
